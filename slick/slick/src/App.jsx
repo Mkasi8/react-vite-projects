04d4d78417a8b06315a7755aa05b70fe898fcfda
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2024-07-04 12:09:28 +0200

    Overwriting the slick library css on the image

---
+++ slick/slick/src/App.jsx
@@ -0,0 +1,11 @@
+import viteLogo from "/vite.svg";
+import SlickCarousel from "./Slick";
+function App() {
+  return (
+    <>
+      <SlickCarousel />
+    </>
+  );
+}
+
+export default App;
---
+++ slick/slick/src/Slick.jsx
@@ -0,0 +1,38 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { longList } from "./data.js";
+import { FaQuoteLeft } from "react-icons/fa6";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const SlickCarousel = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Slider {...settings} className="slick-container">
+      {longList.map((singlePerson) => {
+        //Destructuring the singlePerson object
+        const { id, image, name, title, quote } = singlePerson;
+
+        //Html for displaying the single person
+        return (
+          <div key={id}>
+            <img src={image} alt={name} className="image" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteLeft className="icon" />
+          </div>
+        );
+      })}
+    </Slider>
+  );
+};
+
+export default SlickCarousel;
